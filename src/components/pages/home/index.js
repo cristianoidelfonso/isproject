37a2750e979fd97d272d855659821e52c1730596
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-01-21 09:08:35 -0300

    Criando a Home

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,6 +1,15 @@
+import savings from '../../../img/savings.svg'
+import LinkButton from '../../layouts/LinkButton';
+import './style.css'
+
 function Home() {
   return(
-    <h1>Home</h1>
+    <section className='home_container'>
+      <h1>Bem vindo ao <span>Is Project</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar Projeto" />
+      <img src={savings} alt="Is Project"/>
+    </section>
   );
 }
 

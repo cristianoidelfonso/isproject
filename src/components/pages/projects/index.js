8cebaca7cd8933dc424632a37cdb3d40f50963b1
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-01-25 09:27:29 -0300

    populando a dashboard

--- src/components/pages/projects/index.js
+++ src/components/pages/projects/index.js
@@ -4,8 +4,12 @@ import Container from "../../layouts/Container";
 import LinkButton from "../../layouts/LinkButton";
 
 import styles from  './style.module.css';
+import ProjectCard from "../../project/ProjetcCard";
+import { useState, useEffect } from "react";
 
 function Projects() {
+
+  const [projects, setProjects] = useState([]);
 
   const location = useLocation();
 
@@ -14,6 +18,23 @@ function Projects() {
   if(location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+
+    fetch('http://localhost:5000/projects', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setProjects(data);
+    })
+    .catch((error) => console.log(error))
+
+  }, []);
 
   return(
     <div className={styles.project_container}>
@@ -24,7 +45,18 @@ function Projects() {
       {message && <Message type="success" message={message} />}
       
       <Container customClass="start">
-        <p>Projetos ...</p>
+        {
+          projects.length > 0 &&
+            projects.map((project) => (
+              <ProjectCard 
+                key={project.id} 
+                id={project.id} 
+                name={project.name} 
+                budget={project.budget} 
+                category={project.category.name}  
+              />
+            ))
+        }
       </Container>
 
     </div>   
